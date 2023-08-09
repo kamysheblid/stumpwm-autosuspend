78f3d7e1cb3444c771bbc31133ab495281a32f30
kamysheblid <kamysheblid@gmail.com>
2023-08-09 22:05:38 +0330

    Include update-values in exported fns

--- package.lisp
+++ package.lisp
@@ -4,6 +4,6 @@
   (:use #:cl)
   (:import-from #:stumpwm #:run-shell-command #:cancel-timer #:run-with-timer #:*timer-list*)
   (:export  
-   #:start-loop
+   #:start-loop #:update-values
    #:*battery-status-path* #:*battery-capacity-path* #:*notify-time*  #:*percent-to-suspend* #:*percent-to-notify* #:*notification-title* #:*notification-body*
    ))
